1c69091d5d02d4debac0ca656e2bbc49cab7dd0b
Benjamin Seregi <benjamin@omnetpp.org>
2014-10-20 17:26:20 +0200

    IErrorModel: ..

--- src/inet/physicallayer/contract/IErrorModel.h
+++ src/inet/physicallayer/contract/IErrorModel.h
@@ -42,7 +42,7 @@ class INET_API IErrorModel : public IPrintableObject
 class INET_API ILayeredErrorModel : public IErrorModel
 {
   public:
-    virtual IReceptionPacketModel *computePacketModel(const ISNIR *snir) const = 0;
+    virtual IReceptionPacketModel *computePacketModel(const ITransmissionPacketModel *transmissionBitModel, const ISNIR *snir) const = 0;
     virtual IReceptionBitModel *computeBitModel(const ITransmissionBitModel *transmissionBitModel, const ISNIR *snir) const = 0;
     virtual IReceptionSymbolModel *computeSymbolModel(const ITransmissionSymbolModel *transmissionSymbolModel, const ISNIR *snir) const = 0;
     virtual IReceptionSampleModel *computeSampleModel(const ITransmissionSampleModel *tranmssionSampleModel, const ISNIR *snir) const = 0;
