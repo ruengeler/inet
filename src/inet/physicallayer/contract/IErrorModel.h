ffc1c23685b09cdd8789fc328874d569da006a45
Benjamin Seregi <benjamin@omnetpp.org>
2014-10-31 13:14:49 +0100

    IErrorModel: should not inherit from IErrorModel.

--- src/inet/physicallayer/contract/IErrorModel.h
+++ src/inet/physicallayer/contract/IErrorModel.h
@@ -36,7 +36,7 @@ class INET_API IErrorModel : public IPrintableObject
 };
 
 // TODO: move ILayeredErrorModel after the layered radio is merged in
-class INET_API ILayeredErrorModel : public IErrorModel
+class INET_API ILayeredErrorModel
 {
   public:
     virtual const IReceptionPacketModel *computePacketModel(const LayeredTransmission *transmission, const ISNIR *snir) const = 0;
