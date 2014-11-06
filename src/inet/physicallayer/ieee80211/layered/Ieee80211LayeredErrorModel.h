eb2438c1b5382e06fcca67bbcf9bf30c5599bab3
Benjamin Seregi <benjamin@omnetpp.org>
2014-11-06 14:05:49 +0100

    Ieee80211LayeredErrorModel: fixed a typo. Added printToStream().

--- src/inet/physicallayer/ieee80211/layered/Ieee80211LayeredErrorModel.h
+++ src/inet/physicallayer/ieee80211/layered/Ieee80211LayeredErrorModel.h
@@ -35,6 +35,7 @@ class INET_API Ieee80211LayeredErrorModel : public LayeredErrorModel
         virtual const IReceptionBitModel *computeBitModel(const LayeredTransmission *transmission, const ISNIR *snir) const;
         virtual const IReceptionSymbolModel *computeSymbolModel(const LayeredTransmission *transmission, const ISNIR *snir) const;
         virtual const IReceptionSampleModel *computeSampleModel(const LayeredTransmission *transmission, const ISNIR *snir) const;
+        virtual void printToStream(std::ostream& stream) const { stream << "Ieee80211 Layered Error Model"; }
 };
 
 } /* namespace physicallayer */
