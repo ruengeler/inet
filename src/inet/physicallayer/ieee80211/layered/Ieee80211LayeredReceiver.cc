31c9fa43f6efeb6e0cecfc12ef8e475bd2e00a81
Benjamin Seregi <benjamin@omnetpp.org>
2014-11-20 11:54:43 +0100

    Ieee80211LayeredReceiver: added support for compliant and non-compliant modes.

--- src/inet/physicallayer/ieee80211/layered/Ieee80211LayeredReceiver.cc
+++ src/inet/physicallayer/ieee80211/layered/Ieee80211LayeredReceiver.cc
@@ -18,21 +18,105 @@
 #include "inet/physicallayer/ieee80211/layered/Ieee80211LayeredReceiver.h"
 #include "inet/physicallayer/contract/ISNIR.h"
 #include "inet/physicallayer/contract/IErrorModel.h"
+#include "inet/physicallayer/contract/ISymbol.h"
 #include "inet/physicallayer/layered/LayeredReceiver.h"
 #include "inet/physicallayer/layered/LayeredScalarTransmission.h"
 #include "inet/physicallayer/layered/LayeredReceptionDecision.h"
 #include "inet/physicallayer/layered/LayeredScalarReception.h"
-#include "inet/physicallayer/layered/SignalPacketModel.h"
 #include "inet/physicallayer/layered/SignalSymbolModel.h"
 #include "inet/physicallayer/layered/SignalSampleModel.h"
 #include "inet/physicallayer/analogmodel/layered/SignalAnalogModel.h"
 #include "inet/physicallayer/common/BandListening.h"
-#include "inet/physicallayer/contract/IRadioMedium.h"
+#include "inet/physicallayer/ieee80211/layered/Ieee80211LayeredDecoder.h"
+#include "inet/physicallayer/ieee80211/layered/Ieee80211OFDMDemodulator.h"
+#include "inet/physicallayer/modulation/QAM16Modulation.h"
+#include "inet/physicallayer/modulation/QAM64Modulation.h"
+#include "inet/physicallayer/modulation/BPSKModulation.h"
+#include "inet/physicallayer/modulation/QPSKModulation.h"
 
 namespace inet {
 namespace physicallayer {
 
+#define OFDM_SYMBOL_SIZE 48
+
 Define_Module(Ieee80211LayeredReceiver);
+
+void Ieee80211LayeredReceiver::initialize(int stage)
+{
+    if (stage == INITSTAGE_LOCAL)
+    {
+        errorModel = dynamic_cast<ILayeredErrorModel *>(getSubmodule("errorModel"));
+        decoder = dynamic_cast<IDecoder *>(getSubmodule("decoder"));
+        headerDecoder = dynamic_cast<IDecoder *>(getSubmodule("headerDecoder"));
+        demodulator = dynamic_cast<IDemodulator *>(getSubmodule("demodulator"));
+        headerDemodulator = dynamic_cast<IDemodulator *>(getSubmodule("headerDemodulator"));
+        pulseFilter = dynamic_cast<IPulseFilter *>(getSubmodule("pulseFilter"));
+        analogDigitalConverter = dynamic_cast<IAnalogDigitalConverter *>(getSubmodule("analogDigitalConverter"));
+
+        energyDetection = mW(math::dBm2mW(par("energyDetection")));
+        // TODO: temporary parameters
+        sensitivity = mW(math::dBm2mW(par("sensitivity")));
+        carrierFrequency = Hz(par("carrierFrequency"));
+        bandwidth = Hz(par("bandwidth"));
+        snirThreshold = math::dB2fraction(par("snirThreshold"));
+        channelSpacing = Hz(par("channelSpacing"));
+    }
+}
+
+const IReceptionSymbolModel* Ieee80211LayeredReceiver::createSignalFieldReceptionSymbolModel(const IReceptionSymbolModel* receptionSymbolModel) const
+{
+//    return new ReceptionSymbolModel(transmissionSymbolModel->getSymbolLength(), transmissionSymbolModel->getSymbolRate(), corruptedSymbols);
+    const std::vector<const ISymbol *> *symbols = receptionSymbolModel->getSymbols();
+    std::vector<const ISymbol *> *signalSymbols = new std::vector<const ISymbol *>(); // FIXME: memory leak
+    signalSymbols->push_back(symbols->at(0)); // The first symbol is the signal field symbol
+    // TODO: revise symbolLength, symbolRate  !!
+    return new ReceptionSymbolModel(1, receptionSymbolModel->getSymbolRate(), signalSymbols);
+}
+
+const IReceptionPacketModel *Ieee80211LayeredReceiver::demodulateAndDecodeSignalField(const IRadioMedium *medium, const IRadio *receiver, const LayeredScalarTransmission *transmission, const IReceptionSymbolModel *receptionSymbolModel) const
+{
+    const IReceptionSymbolModel *signalFieldReceptionSymbolModel = NULL;
+    const IReceptionBitModel *signalFieldReceptionBitModel = NULL;
+    const IReceptionPacketModel *signalFieldReceptionPacketModel = NULL;
+    if (levelOfDetail >= SYMBOL_DOMAIN)
+    {
+        if (!receptionSymbolModel)
+        {
+            const ISNIR *snir = medium->getSNIR(receiver, transmission);
+            ASSERT(transmission->getSymbolModel() != NULL);
+            receptionSymbolModel = errorModel->computeSymbolModel(transmission, snir);
+            signalFieldReceptionSymbolModel = createSignalFieldReceptionSymbolModel(receptionSymbolModel);
+        }
+        if (headerDemodulator) // non-compliant mode
+            signalFieldReceptionBitModel = headerDemodulator->demodulate(signalFieldReceptionSymbolModel);
+        else
+        {
+            // In compliant mode, the signal field modulation is always BPSK
+            const Ieee80211OFDMDemodulator demodulator(&BPSKModulation::singleton);
+            signalFieldReceptionBitModel = demodulator.demodulate(signalFieldReceptionSymbolModel);
+        }
+    }
+    if (levelOfDetail >= BIT_DOMAIN)
+    {
+        if (!signalFieldReceptionBitModel)
+        {
+            const ISNIR *snir = medium->getSNIR(receiver, transmission);
+            ASSERT(transmission->getBitModel() != NULL);
+            signalFieldReceptionBitModel = errorModel->computeBitModel(transmission, snir);
+        }
+        if (headerDecoder) // non-compliant mode
+            signalFieldReceptionPacketModel = decoder->decode(signalFieldReceptionBitModel);
+        else
+        {
+            // In compliant mode, the default decoder parameters are as follows
+            const Ieee80211LayeredDecoder decoder(new Ieee80211Scrambler(new Ieee80211Scrambling("1011101", "0001001")),
+                                            new ConvolutionalCoder(new Ieee80211ConvolutionalCode(1,2)),
+                                            new Ieee80211Interleaver(new Ieee80211Interleaving(BPSKModulation::singleton.getCodeWordLength() * OFDM_SYMBOL_SIZE, BPSKModulation::singleton.getCodeWordLength())));
+            signalFieldReceptionPacketModel = decoder.decode(signalFieldReceptionBitModel);
+        }
+    }
+    return signalFieldReceptionPacketModel;
+}
 
 const IReceptionDecision *Ieee80211LayeredReceiver::computeReceptionDecision(const IListening *listening, const IReception *reception, const IInterference *interference) const
 {
@@ -65,7 +149,8 @@ const IReceptionDecision *Ieee80211LayeredReceiver::computeReceptionDecision(con
         receptionIndication->setMinRSSI(receptionSampleModel->getRSSI());
         receptionSymbolModel = pulseFilter->filter(receptionSampleModel);
     }
-    if (demodulator)
+    const IReceptionPacketModel *signalFieldReceptionPacketModel = demodulateAndDecodeSignalField(medium, receiver, transmission, receptionSymbolModel);
+    if (headerDemodulator)
     {
         if (!receptionSymbolModel)
         {
@@ -76,7 +161,7 @@ const IReceptionDecision *Ieee80211LayeredReceiver::computeReceptionDecision(con
 //        FIXME: delete ser from reception indication?
 //        receptionIndication->setSymbolErrorCount(receptionSymbolModel->getSymbolErrorCount());
 //        receptionIndication->setSymbolErrorRate(receptionSymbolModel->getSER());
-        receptionBitModel = demodulator->demodulate(receptionSymbolModel);
+        receptionBitModel = headerDemodulator->demodulate(receptionSymbolModel);
     }
     if (decoder)
     {
@@ -96,7 +181,7 @@ const IReceptionDecision *Ieee80211LayeredReceiver::computeReceptionDecision(con
     receptionIndication->setPacketErrorRate(receptionPacketModel->getPER());
     // FIXME: Kludge: we have no serializer yet
     const cPacket *macFrame = transmission->getMacFrame();
-    const ReceptionPacketModel *hackedPacketModel = new ReceptionPacketModel(macFrame, receptionPacketModel->getForwardErrorCorrection(),
+    const ReceptionPacketModel *hackedPacketModel = new ReceptionPacketModel(macFrame, receptionPacketModel->getSerializedPacket(), receptionPacketModel->getForwardErrorCorrection(),
                                                                               receptionPacketModel->getScrambling(), receptionPacketModel->getInterleaving(),
                                                                               receptionPacketModel->getPER(), receptionPacketModel->isPacketErrorless());
     return new LayeredReceptionDecision(reception, receptionIndication, hackedPacketModel, NULL, NULL, NULL, NULL, true, true, hackedPacketModel->isPacketErrorless());
--- src/inet/physicallayer/ieee80211/layered/Ieee80211LayeredReceiver.h
+++ src/inet/physicallayer/ieee80211/layered/Ieee80211LayeredReceiver.h
@@ -19,13 +19,22 @@
 #define __INET_IEEE80211LAYEREDRECEIVER_H
 
 #include "inet/physicallayer/layered/LayeredReceiver.h"
-#include "inet/physicallayer/layered/SignalBitModel.h"
+#include "inet/physicallayer/layered/SignalPacketModel.h"
+#include "inet/physicallayer/contract/IRadioMedium.h"
 
 namespace inet {
 namespace physicallayer {
 
 class INET_API Ieee80211LayeredReceiver : public LayeredReceiver
 {
+    protected:
+        Hz channelSpacing;
+
+    protected:
+        virtual void initialize(int stage);
+        const IReceptionSymbolModel *createSignalFieldReceptionSymbolModel(const IReceptionSymbolModel *receptionSymbolModel) const;
+        const IReceptionPacketModel *demodulateAndDecodeSignalField(const IRadioMedium *medium, const IRadio *receiver, const LayeredScalarTransmission *transmission, const IReceptionSymbolModel *receptionSymbolModel) const;
+
     public:
         virtual const IReceptionDecision *computeReceptionDecision(const IListening *listening, const IReception *reception, const IInterference *interference) const;
 };
