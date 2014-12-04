1e2519575524ca4e216dd1cd29e3af33dd95d29e
Benjamin Seregi <benjamin@omnetpp.org>
2014-12-04 15:45:54 +0100

    Ieee80211ScramblerModule: code style.

--- src/inet/physicallayer/ieee80211/layered/Ieee80211ScramblerModule.h
+++ src/inet/physicallayer/ieee80211/layered/Ieee80211ScramblerModule.h
@@ -13,7 +13,8 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program; if not, see <http://www.gnu.org/licenses/>.
-// 
+//
+
 #ifndef __INET_IEEE80211SCRAMBLERMODULE_H
 #define __INET_IEEE80211SCRAMBLERMODULE_H
 
