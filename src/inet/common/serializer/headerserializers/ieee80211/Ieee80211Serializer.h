9d9ff130a217c50ccab86cb69db8829f3edb1bbb
Benjamin Seregi <benjamin@omnetpp.org>
2014-12-05 14:01:14 +0100

    Ieee80211Serializer: copyright & header guards & INET_API.

--- src/inet/common/serializer/headerserializers/ieee80211/Ieee80211Serializer.h
+++ src/inet/common/serializer/headerserializers/ieee80211/Ieee80211Serializer.h
@@ -1,20 +1,22 @@
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
+// Copyright (C) 2014 OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
 
-#ifndef IEEE80211SERIALIZER_H_
-#define IEEE80211SERIALIZER_H_
+#ifndef __INET_IEEE80211SERIALIZER_H
+#define __INET_IEEE80211SERIALIZER_H
 
 #include "inet/linklayer/ieee80211/mac/Ieee80211Frame_m.h"
 #include "inet/common/serializer/headers/defs.h"
@@ -22,17 +24,14 @@
 /**
  * Converts between Ieee802.11Frame and binary (network byte order)  Ieee802.11 header.
  */
-
 namespace inet {
-
 namespace serializer {
 
 using namespace ieee80211;
 
-class Ieee80211Serializer
+class INET_API Ieee80211Serializer
 {
     public:
-
         /**
          * Serializes an Ieee802.11Frame for transmission on the network card.
          * Returns the length of data written into buffer.
@@ -49,4 +48,4 @@ class Ieee80211Serializer
 
 } // namespace inet
 
-#endif /* IEEE80211SERIALIZER_H_ */
+#endif /* __INET_IEEE80211SERIALIZER_H */
