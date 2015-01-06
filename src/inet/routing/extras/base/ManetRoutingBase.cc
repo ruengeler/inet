90b56efedafee2f6d23c97939c1cb71b67525831
Zoltan Bojthe <zbojthe@omnetpp.org>
2015-01-06 13:26:02 +0100

    getParentModule: manetrouting: subscribe in node for mobility state changes

--- src/inet/routing/extras/base/ManetRoutingBase.cc
+++ src/inet/routing/extras/base/ManetRoutingBase.cc
@@ -372,9 +372,7 @@ void ManetRoutingBase::registerPosition()
     if (!isRegistered)
         throw cRuntimeError("Manet routing protocol is not register");
     regPosition = true;
-    cModule *mod = findContainingNode(getParentModule());
-    if (!mod)
-        mod = getParentModule();
+    cModule *mod = getContainingNode(this);
     mod->subscribe(mobilityStateChangedSignal, this);
 }
 
